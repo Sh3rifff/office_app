8848879923dd031eda551b8cb5a1bb3e86c09679
Serif <serifsireliyev2002@gmail.com>
2023-04-10 11:36:49 +0400

    ssalambla

---
+++ app/src/main/java/com/uniser/officeapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.uniser.officeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
