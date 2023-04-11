2d66854ce056b2858cf20eeabf1ccd0307877c18
Jafar Tarverdiyev <cefertarverdiyevv@gmail.com>
2023-04-11 11:49:48 +0400

    RoundedLayout

--- app/src/main/java/com/uniser/officeapp/view/MainActivity.kt
+++ app/src/main/java/com/uniser/officeapp/view/MainActivity.kt
@@ -7,6 +7,6 @@ import com.uniser.officeapp.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_fragment)
     }
 }
